1a2d4fa60adccc0147c75aace4b3e9920671a5a5
ArdaAsci <abdullah-arda@hotmail.com>
2021-10-26 00:47:06 +0300

    Some refactoring

--- src/loops.py
+++ src/loops.py
@@ -7,10 +7,16 @@ class Loop:
     If nested, it has a reference to its parent loop a list of child loops.
     """
 
-    def __init__(self, name, iteration_count: int = 5, parent_loop=None) -> None:
+    def __init__(
+        self, name, iteration_count: int = 5, parent_loop=None, rank=0
+    ) -> None:
         self.name = name
         self.iteration_count = iteration_count
         self.parent_loop: Loop = parent_loop
+        if self.parent_loop == None:
+            self.rank = rank
+        else:
+            self.rank = self.parent_loop.rank
         self.child_loops: List[Loop] = []
 
     def add_child(self, child):
@@ -29,10 +35,24 @@ class Loop:
         return False
 
     @property
-    def rank(self):
-        rank = 0
+    def nested_number(self):
+        """
+        Which layer this loop is in its nested loops.
+        0 for top level
+        """
+        nested_number = 0
         parent = self.parent_loop
         while parent is not None:
-            rank += 1
+            nested_number += 1
             parent = parent.parent_loop
-        return rank
+        return nested_number
+
+    @property
+    def total_nested_depth(self):
+        """
+        How many layers there are in this loops nested loops.
+        """
+        if self.child_loops == []:
+            return self.nested_number
+        child_depth = [child.total_nested_depth for child in self.child_loops]
+        return max(child_depth)
